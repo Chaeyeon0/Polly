13d6898231149da441b0c88320b01255ee965a05
qorcodusl <qorcodus03@naver.com>
2025-05-09 17:18:53 +0900

    Initial commit

---
+++ src/test/java/com/example/polly_system/PollySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polly_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
